503ee193c86badc079b8ae8fc3ff32c039d7fbea
Suraj Patankar <psuraj.2650@gmail.com>
2026-06-01 17:50:32 +0530

    The program takes an input of three values from the user and returns their sum.

--- Programs/Sum_of_three_values.java
+++ Programs/Sum_of_three_values.java
@@ -4,11 +4,11 @@ public class Sum_of_three_values {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
         System.out.println("The sum of three values");
-        System.out.print("Enter first value: ");
+        System.out.print("Enter first value:");
         float a = input.nextFloat();
-        System.out.print("Enter second value: ");
+        System.out.print("Enter second value:");
         float b = input.nextFloat();
-        System.out.print("Enter third value: ");
+        System.out.print("Enter third value:");
         float c = input.nextFloat();
         float sum = a+b+c;
         System.out.println("The sum of three value is: " +sum);
