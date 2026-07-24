bbb5ba3f824d68f3414784433bda3908f980e7ef
Suraj Patankar <psuraj.2650@gmail.com>
2026-07-24 14:09:46 +0530

    The program takes an input of a Character from the user and checks whether the input is lower case or upper case.

---
+++ Programs/Case_check.java
@@ -0,0 +1,14 @@
+package Programs;
+import java.util.Scanner;
+public class Case_check {
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an Alphabet: ");
+        char ch = in.next().trim().charAt(0);
+        if(ch >= 'a' && ch <= 'z'){
+            System.out.println("The Alphabet entered is a lower case");
+        }
+        else
+            System.out.println("The Alphabet entered UPPER CASE");
+    }
+}
