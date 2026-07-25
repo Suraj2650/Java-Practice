12e73825d0e1aaff36484e1401683ab05fa815e9
Suraj Patankar <psuraj.2650@gmail.com>
2026-07-25 14:10:06 +0530

    The program takes an input of Fibonacci iterations (nth term) from the user and returns the entire Fibonacci series till the nth term.

--- Programs/Fibonacci_series.java
+++ Programs/Fibonacci_series.java
@@ -1,17 +1,18 @@
 package Programs;
 import java.util.Scanner;
-public class Fibonacci {
+public class Fibonacci_series {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.print("Enter number of iterations(n): ");
         int n = in.nextInt();
         int a = 0;
         int b = 1;
-        for(int count = 2;count <= n; count++){
+        System.out.println("Fibonacci series: ");
+        for(int count = 1;count <= n; count++){
+            System.out.println(a + " ");
             int temp = b;
             b = b + a;
             a = temp;
         }
-        System.out.println("The number at nth term is:" + b);
     }
 }
